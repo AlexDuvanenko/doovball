c15f2db78e8cbc8eb8a093de687f534f0a1be924
Alex Duvanenko <ajduvanenko@gmail.com>
2023-10-18 12:26:01 -0400

    league team updates
    - updated league team to an autocomplete to dynamically pass options
    - updated interactionCreate to accept and handle autocomplete

--- commands/utility/leagueTeam.js
+++ commands/utility/leagueTeam.js
@@ -1,16 +1,15 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { fetchLeagueTeams } = require("../../api/teamsInfoAPI");
-const { Global } = require("espn-fantasy-football-api/node");
 
 const WEEK_ID = 18; // 18 refers to end of season
-const SELECTED_TEAM_ID = 1;
 
-const TEAM_EMBED = {
+const INIITIAL_TEAM_EMBED = {
     color: 0x0099ff,
+    description: "",
     fields: [],
 };
 
-const formatLeagueTeamsData = async () => {
+const getLeagueTeamsData = async () => {
     try {
         const teamsData = await fetchLeagueTeams(WEEK_ID);
         if (!teamsData) {
@@ -18,42 +17,80 @@ const formatLeagueTeamsData = async () => {
             return;
         }
 
-        teamsData.forEach((team) => {
-            if (team.id === SELECTED_TEAM_ID) {
-                console.log(Global);
-                TEAM_EMBED.title = team.name;
-                TEAM_EMBED.thumbnail = { url: team.logoURL };
-                TEAM_EMBED.description = `(${team.wins}-${team.losses}-${team.ties})`;
-                let possibleInactiveCount = 0;
-
-                team.roster.forEach((player) => {
-                    if (
-                        player.injuryStatus &&
-                        player.injuryStatus !== "ACTIVE"
-                    ) {
-                        possibleInactiveCount++;
-                    }
-                });
-
-                TEAM_EMBED.fields.push({
-                    name: "Possible Inactives",
-                    value: `${possibleInactiveCount}`,
-                });
-            }
-        });
+        return teamsData;
     } catch (error) {
         console.error("Error: ", error);
     }
+};
+
+const formatTeamsDataForAutocomplete = async () => {
+    const data = await getLeagueTeamsData();
+    const teams = [];
+
+    data.forEach((team) => {
+        teams.push({ name: `${team.name}`, value: `${team.id}` });
+    });
+
+    return teams;
+};
+
+const createTeamEmbed = async (teamId) => {
+    const teams = await getLeagueTeamsData();
+    console.log("teams", teams);
+    const teamEmbed = INIITIAL_TEAM_EMBED;
+
+    const filteredTeam = teams.find((team) => team.id == teamId);
+
+    if (!filteredTeam) {
+        teamEmbed.description = "No data for selected team!";
+        return teamEmbed;
+    }
+
+    const pointsFields = [
+        {
+            name: "Points Scored",
+            value: `**${filteredTeam.regularSeasonPointsFor.toFixed(2)}**`,
+            inline: true,
+        },
+        {
+            name: "Points Against",
+            value: `**${filteredTeam.regularSeasonPointsAgainst.toFixed(2)}**`,
+            inline: true,
+        },
+    ];
+    teamEmbed.title = filteredTeam.name;
+    // TODO: if svg, convert image to something discord can embed
+    teamEmbed.thumbnail = { url: filteredTeam.logoURL };
+    teamEmbed.description = `Record: **(${filteredTeam.wins}-${filteredTeam.losses}-${filteredTeam.ties})**`;
+    pointsFields.forEach((field) => teamEmbed.fields.push(field));
+
+    return teamEmbed;
 };
 
 module.exports = {
     cooldown: 5,
     data: new SlashCommandBuilder()
         .setName("league-team")
-        .setDescription("League team"),
+        .setDescription("Information about a specific team in the league")
+        .addStringOption((option) =>
+            option
+                .setName("team")
+                .setDescription("The specific team")
+                .setRequired(true)
+                .setAutocomplete(true)
+        ),
+    async autocomplete(interaction) {
+        const teams = await formatTeamsDataForAutocomplete();
+        const focusedValue = interaction.options.getFocused();
+        const filtered = teams.filter((choice) =>
+            choice.name.startsWith(focusedValue)
+        );
+        await interaction.respond(filtered);
+    },
     async execute(interaction) {
         await interaction.deferReply();
-        await formatLeagueTeamsData(WEEK_ID);
-        await interaction.editReply({ embeds: [TEAM_EMBED] });
+        const selectedTeam = interaction.options.getString("team");
+        const team = await createTeamEmbed(selectedTeam);
+        await interaction.editReply({ embeds: [team] });
     },
 };
